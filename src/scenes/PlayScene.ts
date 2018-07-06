1000a90c9908b9e53a458357d057106c49c86422
Joshua Levin <joshua.c.levin@gmail.com>
2018-07-07 00:45:31 +0100

    tidy code

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -14,14 +14,12 @@ class TestScene extends Phaser.Scene {
 
 	public create() {
 		const scale = 3;
-		const forestBack = this.add.sprite((272 * scale) / 2, (160 * scale) / 2, 'forest-back');
-		forestBack.setScale(scale, scale);
-		const forestMiddle = this.add.sprite((272 * scale) / 2, (160 * scale) / 2, 'forest-middle');
-		forestMiddle.setScale(scale, scale);
-		const forestFront = this.add.sprite((272 * scale) / 2, (160 * scale) / 2, 'forest-front');
-		forestFront.setScale(scale, scale);
-		const forestLights = this.add.sprite((272 * scale) / 2, (160 * scale) / 2, 'forest-lights');
-		forestLights.setScale(scale, scale);
+		const background: Phaser.GameObjects.Sprite[] = [];
+		background.push(this.add.sprite((272 * scale) / 2, (160 * scale) / 2, 'forest-back'));
+		background.push(this.add.sprite((272 * scale) / 2, (160 * scale) / 2, 'forest-middle'));
+		background.push(this.add.sprite((272 * scale) / 2, (160 * scale) / 2, 'forest-front'));
+		background.push(this.add.sprite((272 * scale) / 2, (160 * scale) / 2, 'forest-lights'));
+		background.forEach((sprite) => sprite.setScale(scale, scale));
 	}
 
 	public update(time: number, delta: number) {
