cec1d3b946a927925c06144b778f7af38eeae4bd
Joshua Levin <joshua.c.levin@gmail.com>
2018-07-11 00:05:55 +0100

    remove errors

--- src/phaser.d.ts
+++ src/phaser.d.ts
@@ -23842,7 +23842,7 @@ declare namespace Phaser {
              */
             audioSprite(key: string, urls: string | string[], json: object, config: object, audioXhrSettings: object, jsonXhrSettings: object): Phaser.Loader.LoaderPlugin;
 
-            spritesheet(key: string | Phaser.Loader.FileTypes.SpriteSheetFileConfig | Phaser.Loader.FileTypes.SpriteSheetFileConfig[], url?: string, frameConfig?: Phaser.Loader.FileTypes.ImageFrameConfig, xhrSettings?: XHRSettingsObject): Phaser.Loader.LoaderPlugin;
+            spritesheet(key: string, url?: string, frameConfig?: any, xhrSettings?: any): Phaser.Loader.LoaderPlugin;
 
             /**
              * Adds Binary file to the current load queue.
